22b54675510c9841cf1b3da8ff04a643ff4284b5
Nopana_Eliyaan <nopana1007@gmail.com>
2025-12-31 14:27:07 +0100

    move in module

--- main.v
+++ main.v
@@ -4,16 +4,10 @@ import pbp_graph as pg
 @[heap]
 struct App {
 mut:
-	ctx              &gg.Context = unsafe { nil }
-	selected_i       int         = -1 // selected link
-	selected_variant pg.Variant
-	l_start          bool // is the input or the output of the link is selected/controled
-	b_dx             f32  // offset from block_x to mouse click_x
-	b_dy             f32
-	v                pg.View
-	mouse_clicked    bool
-	mouse_x          f32
-	mouse_y          f32
+	ctx     &gg.Context = unsafe { nil }
+	v       pg.View
+	mouse_x f32
+	mouse_y f32
 }
 
 fn main() {
@@ -34,210 +28,8 @@ fn main() {
 
 fn on_event(e &gg.Event, mut app App) {
 	app.mouse_x = e.mouse_x
+	app.v.events(e)
 	app.mouse_y = e.mouse_y
-	match e.typ {
-		.mouse_move {
-			if app.mouse_clicked && app.selected_i != -1 {
-				if app.selected_variant == .link {
-					p_i := app.v.which_port_is_clicked(e.mouse_x, e.mouse_y)
-					// move already selected link
-					if app.l_start {
-						if p_i != -1 {
-							if !app.v.p_input[p_i] {
-								app.v.l_start_x[app.selected_i] = app.v.p_x[p_i]
-								app.v.l_start_y[app.selected_i] = app.v.p_y[p_i]
-							} else {
-								app.v.l_start_x[app.selected_i] = e.mouse_x
-								app.v.l_start_y[app.selected_i] = e.mouse_y
-							}
-						} else {
-							app.v.l_start_x[app.selected_i] = e.mouse_x
-							app.v.l_start_y[app.selected_i] = e.mouse_y
-						}
-					} else {
-						if p_i != -1 {
-							if app.v.p_input[p_i] {
-								app.v.l_end_x[app.selected_i] = app.v.p_x[p_i]
-								app.v.l_end_y[app.selected_i] = app.v.p_y[p_i]
-							} else {
-								app.v.l_end_x[app.selected_i] = e.mouse_x
-								app.v.l_end_y[app.selected_i] = e.mouse_y
-							}
-						} else {
-							app.v.l_end_x[app.selected_i] = e.mouse_x
-							app.v.l_end_y[app.selected_i] = e.mouse_y
-						}
-					}
-				} else if app.selected_variant == .block {
-					app.v.b_x[app.selected_i] = e.mouse_x - app.b_dx
-					app.v.b_y[app.selected_i] = e.mouse_y - app.b_dy
-					for i, idx in app.v.b_inps[app.selected_i] {
-						app.v.p_x[idx] = app.v.b_x[app.selected_i]
-						app.v.p_y[idx] = app.v.b_y[app.selected_i] + i * app.v.input_spacing +
-							app.v.input_spacing / 2
-						for l in app.v.p_link[idx] {
-							app.v.l_end_x[l] = app.v.p_x[idx]
-							app.v.l_end_y[l] = app.v.p_y[idx]
-						}
-					}
-					for i, idx in app.v.b_outs[app.selected_i] {
-						app.v.p_x[idx] = app.v.b_x[app.selected_i] + app.v.b_w[app.selected_i]
-						app.v.p_y[idx] = app.v.b_y[app.selected_i] + i * app.v.input_spacing +
-							app.v.input_spacing / 2
-						for l in app.v.p_link[idx] {
-							app.v.l_start_x[l] = app.v.p_x[idx]
-							app.v.l_start_y[l] = app.v.p_y[idx]
-						}
-					}
-				}
-			}
-		}
-		.mouse_down {
-			if app.selected_i == -1 {
-				app.mouse_clicked = true
-				p_i := app.v.which_port_is_clicked(e.mouse_x, e.mouse_y)
-				if p_i != -1 {
-					// new link or select link
-					app.selected_variant = .link
-					// create link
-					app.v.p_link[p_i] << app.v.l_inp.len
-					app.selected_i = app.v.l_inp.len
-					if app.v.p_input[p_i] {
-						// we need an input for the link as the input for the block is the output for the link
-						app.l_start = true
-						app.v.l_out << p_i
-						app.v.l_inp << -1
-						app.v.l_end_x << app.v.p_x[p_i]
-						app.v.l_end_y << app.v.p_y[p_i]
-						app.v.l_start_x << e.mouse_x
-						app.v.l_start_y << e.mouse_y
-					} else {
-						app.l_start = false
-						app.v.l_inp << p_i
-						app.v.l_out << -1
-						app.v.l_start_x << app.v.p_x[p_i]
-						app.v.l_start_y << app.v.p_y[p_i]
-						app.v.l_end_x << e.mouse_x
-						app.v.l_end_y << e.mouse_y
-					}
-				} else {
-					l_i := app.v.which_link_is_clicked(e.mouse_x, e.mouse_y)
-					if l_i != -1 {
-						// select link
-						app.selected_variant = .link
-						app.selected_i = l_i
-						if e.mouse_x >= app.v.l_start_x[l_i] {
-							if (e.mouse_x - app.v.l_start_x[l_i]) >= (app.v.l_end_x[l_i] - e.mouse_x) {
-								app.l_start = false
-								idx := app.v.p_link[app.v.l_out[l_i]].index(l_i)
-								if idx != -1 {
-									app.v.p_link[app.v.l_out[l_i]].delete(idx)
-								}
-								app.v.l_out[l_i] = -1
-							} else {
-								app.l_start = true
-								idx := app.v.p_link[app.v.l_inp[l_i]].index(l_i)
-								if idx != -1 {
-									app.v.p_link[app.v.l_inp[l_i]].delete(idx)
-								}
-								app.v.l_inp[l_i] = -1
-							}
-						} else {
-							if (e.mouse_x - app.v.l_end_x[l_i]) <= (app.v.l_start_x[l_i] - e.mouse_x) {
-								app.l_start = false
-								idx := app.v.p_link[app.v.l_out[l_i]].index(l_i)
-								if idx != -1 {
-									app.v.p_link[app.v.l_out[l_i]].delete(idx)
-								}
-								app.v.l_out[l_i] = -1
-							} else {
-								app.l_start = true
-								idx := app.v.p_link[app.v.l_inp[l_i]].index(l_i)
-								if idx != -1 {
-									app.v.p_link[app.v.l_inp[l_i]].delete(idx)
-								}
-								app.v.l_inp[l_i] = -1
-							}
-						}
-					} else {
-						b_i := app.v.which_block_is_clicked(e.mouse_x, e.mouse_y)
-						if b_i != -1 {
-							// select block
-							app.selected_variant = .block
-							app.selected_i = b_i
-							app.b_dx = e.mouse_x - app.v.b_x[b_i]
-							app.b_dy = e.mouse_y - app.v.b_y[b_i]
-						}
-					}
-				}
-			}
-		}
-		.mouse_up {
-			app.mouse_clicked = false
-			if app.selected_i != -1 {
-				if app.selected_variant == .link {
-					p_i := app.v.which_port_is_clicked(e.mouse_x, e.mouse_y)
-					if p_i == -1 || p_i == app.v.l_out[app.selected_i]
-						|| p_i == app.v.l_inp[app.selected_i]
-						|| (p_i != -1 && app.v.p_input[p_i] == app.l_start) {
-						if app.v.l_inp[app.selected_i] != -1 {
-							idx := app.v.p_link[app.v.l_inp[app.selected_i]].index(app.selected_i)
-							if idx != -1 {
-								app.v.p_link[app.v.l_inp[app.selected_i]].delete(idx)
-							}
-						}
-						if app.v.l_out[app.selected_i] != -1 {
-							idx := app.v.p_link[app.v.l_out[app.selected_i]].index(app.selected_i)
-							if idx != -1 {
-								app.v.p_link[app.v.l_out[app.selected_i]].delete(idx)
-							}
-						}
-						app.v.l_end_x[app.selected_i] = -1.0
-						app.v.l_end_y[app.selected_i] = -1.0
-						app.v.l_start_x[app.selected_i] = -1.0
-						app.v.l_start_y[app.selected_i] = -1.0
-						app.v.l_inp[app.selected_i] = -1
-						app.v.l_out[app.selected_i] = -1
-					} else {
-						if app.v.p_input[p_i] {
-							app.v.p_link[p_i] << app.selected_i
-							app.v.l_end_x[app.selected_i] = app.v.p_x[p_i]
-							app.v.l_end_y[app.selected_i] = app.v.p_y[p_i]
-							app.v.l_out[app.selected_i] = p_i
-						} else {
-							app.v.p_link[p_i] << app.selected_i
-							app.v.l_start_x[app.selected_i] = app.v.p_x[p_i]
-							app.v.l_start_y[app.selected_i] = app.v.p_y[p_i]
-							app.v.l_inp[app.selected_i] = p_i
-						}
-					}
-				} else if app.selected_variant == .block {
-					app.v.b_x[app.selected_i] = e.mouse_x - app.b_dx
-					app.v.b_y[app.selected_i] = e.mouse_y - app.b_dy
-					for i, idx in app.v.b_inps[app.selected_i] {
-						app.v.p_x[idx] = app.v.b_x[app.selected_i]
-						app.v.p_y[idx] = app.v.b_y[app.selected_i] + i * app.v.input_spacing +
-							app.v.input_spacing / 2
-						for l in app.v.p_link[idx] {
-							app.v.l_end_x[l] = app.v.p_x[idx]
-							app.v.l_end_y[l] = app.v.p_y[idx]
-						}
-					}
-					for i, idx in app.v.b_outs[app.selected_i] {
-						app.v.p_x[idx] = app.v.b_x[app.selected_i] + app.v.b_w[app.selected_i]
-						app.v.p_y[idx] = app.v.b_y[app.selected_i] + i * app.v.input_spacing +
-							app.v.input_spacing / 2
-						for l in app.v.p_link[idx] {
-							app.v.l_start_x[l] = app.v.p_x[idx]
-							app.v.l_start_y[l] = app.v.p_y[idx]
-						}
-					}
-				}
-				app.selected_i = -1
-			}
-		}
-		else {}
-	}
 }
 
 fn on_frame(mut app App) {
@@ -247,7 +39,6 @@ fn on_frame(mut app App) {
 		size:           int(app.v.char_x) * 2
 	}
 	app.ctx.begin()
-	app.v.draw(app.ctx, app.mouse_x, app.mouse_y, app.selected_i, app.selected_variant,
-		cfg, app.l_start)
+	app.v.draw(app.ctx, app.mouse_x, app.mouse_y, cfg)
 	app.ctx.end()
 }
--- pbp_graph/pbp_graph.v
+++ pbp_graph/pbp_graph.v
@@ -30,6 +30,13 @@ pub mut:
 	l_start_y []f32
 	l_end_x   []f32
 	l_end_y   []f32
+	// interactions
+	selected_i       int = -1 // selected link
+	selected_variant Variant
+	l_start          bool // is the input or the output of the link is selected/controled, is the selected end of the link its start or its end (for the data flowing in it)
+	b_dx             f32  // offset from block_x to mouse click_x
+	b_dy             f32
+	mouse_clicked    bool
 }
 
 pub fn (v View) which_link_is_clicked(x f32, y f32) int {
@@ -114,20 +121,19 @@ pub enum Variant {
 	link
 }
 
-pub fn is_a_block_selected(selected_variant Variant, selected_i int) bool {
-	return selected_variant == .block && selected_i != -1
+pub fn (v View) is_a_block_selected() bool {
+	return v.selected_variant == .block && v.selected_i != -1
 }
 
-pub fn is_a_link_selected(selected_variant Variant, selected_i int) bool {
-	return selected_variant == .link && selected_i != -1
+pub fn (v View) is_a_link_selected() bool {
+	return v.selected_variant == .link && v.selected_i != -1
 }
 
 pub fn is_none(i int) bool {
 	return i == -1
 }
 
-// l_start: is the selected end of the link its start or its end (for the data flowing in it)
-pub fn (v View) draw(ctx &gg.Context, mouse_x f32, mouse_y f32, selected_i int, selected_variant Variant, cfg gg.TextCfg, l_start bool) {
+pub fn (v View) draw(ctx &gg.Context, mouse_x f32, mouse_y f32, cfg gg.TextCfg) {
 	p_i := v.which_port_is_clicked(mouse_x, mouse_y)
 	l_i := v.which_link_is_clicked(mouse_x, mouse_y)
 	for i in 0 .. v.b_x.len {
@@ -135,13 +141,13 @@ pub fn (v View) draw(ctx &gg.Context, mouse_x f32, mouse_y f32, selected_i int, 
 		ctx.draw_text(int(v.b_x[i] + v.b_w[i] / 2), int(v.b_y[i] + v.b_h[i] / 2), v.b_name[i],
 			cfg)
 	}
-	if is_a_block_selected(selected_variant, selected_i) {
-		ctx.draw_rect_filled(v.b_x[selected_i], v.b_y[selected_i], v.b_w[selected_i],
-			v.b_h[selected_i], gg.dark_blue)
-		ctx.draw_text(int(v.b_x[selected_i] + v.b_w[selected_i] / 2), int(v.b_y[selected_i] +
-			v.b_h[selected_i] / 2), v.b_name[selected_i], cfg)
+	if v.is_a_block_selected() {
+		ctx.draw_rect_filled(v.b_x[v.selected_i], v.b_y[v.selected_i], v.b_w[v.selected_i],
+			v.b_h[v.selected_i], gg.dark_blue)
+		ctx.draw_text(int(v.b_x[v.selected_i] + v.b_w[v.selected_i] / 2), int(v.b_y[v.selected_i] +
+			v.b_h[v.selected_i] / 2), v.b_name[v.selected_i], cfg)
 	} else {
-		if is_none(p_i) && is_none(l_i) && !is_a_link_selected(selected_variant, selected_i) {
+		if is_none(p_i) && is_none(l_i) && !v.is_a_link_selected() {
 			b_i := v.which_block_is_clicked(mouse_x, mouse_y)
 			if !is_none(b_i) {
 				ctx.draw_rect_filled(v.b_x[b_i], v.b_y[b_i], v.b_w[b_i], v.b_h[b_i], gg.dark_blue)
@@ -154,9 +160,8 @@ pub fn (v View) draw(ctx &gg.Context, mouse_x f32, mouse_y f32, selected_i int, 
 		ctx.draw_circle_filled(v.p_x[i], v.p_y[i], v.radius, gg.light_gray)
 		ctx.draw_text(int(v.p_x[i]), int(v.p_y[i]), v.p_name[i], cfg)
 	}
-	if !is_none(p_i) && !is_a_block_selected(selected_variant, selected_i)
-		&& (!is_a_link_selected(selected_variant, selected_i)
-		|| l_start != v.p_input[p_i]) { // if it is the side it can connect to
+	if !is_none(p_i) && !v.is_a_block_selected()
+		&& (!v.is_a_link_selected() || v.l_start != v.p_input[p_i]) { // if it is the side it can connect to
 		ctx.draw_circle_filled(v.p_x[p_i], v.p_y[p_i], v.radius, gg.dark_gray)
 		ctx.draw_text(int(v.p_x[p_i]), int(v.p_y[p_i]), v.p_name[p_i], cfg)
 	}
@@ -166,15 +171,217 @@ pub fn (v View) draw(ctx &gg.Context, mouse_x f32, mouse_y f32, selected_i int, 
 				gg.black)
 		}
 	}
-	if !is_a_block_selected(selected_variant, selected_i) {
-		if is_a_link_selected(selected_variant, selected_i) {
-			ctx.draw_line(v.l_start_x[selected_i], v.l_start_y[selected_i], v.l_end_x[selected_i],
-				v.l_end_y[selected_i], gg.red)
+	if !v.is_a_block_selected() {
+		if v.is_a_link_selected() {
+			ctx.draw_line(v.l_start_x[v.selected_i], v.l_start_y[v.selected_i], v.l_end_x[v.selected_i],
+				v.l_end_y[v.selected_i], gg.red)
 		} else {
 			if is_none(p_i) && !is_none(l_i) {
 				ctx.draw_line(v.l_start_x[l_i], v.l_start_y[l_i], v.l_end_x[l_i], v.l_end_y[l_i],
 					gg.red)
 			}
 		}
+	}
+}
+
+pub fn (mut v View) events(e &gg.Event) {
+	match e.typ {
+		.mouse_move {
+			if v.mouse_clicked && v.selected_i != -1 {
+				if v.selected_variant == .link {
+					p_i := v.which_port_is_clicked(e.mouse_x, e.mouse_y)
+					// move already selected link
+					if v.l_start {
+						if p_i != -1 {
+							if !v.p_input[p_i] {
+								v.l_start_x[v.selected_i] = v.p_x[p_i]
+								v.l_start_y[v.selected_i] = v.p_y[p_i]
+							} else {
+								v.l_start_x[v.selected_i] = e.mouse_x
+								v.l_start_y[v.selected_i] = e.mouse_y
+							}
+						} else {
+							v.l_start_x[v.selected_i] = e.mouse_x
+							v.l_start_y[v.selected_i] = e.mouse_y
+						}
+					} else {
+						if p_i != -1 {
+							if v.p_input[p_i] {
+								v.l_end_x[v.selected_i] = v.p_x[p_i]
+								v.l_end_y[v.selected_i] = v.p_y[p_i]
+							} else {
+								v.l_end_x[v.selected_i] = e.mouse_x
+								v.l_end_y[v.selected_i] = e.mouse_y
+							}
+						} else {
+							v.l_end_x[v.selected_i] = e.mouse_x
+							v.l_end_y[v.selected_i] = e.mouse_y
+						}
+					}
+				} else if v.selected_variant == .block {
+					v.b_x[v.selected_i] = e.mouse_x - v.b_dx
+					v.b_y[v.selected_i] = e.mouse_y - v.b_dy
+					for i, idx in v.b_inps[v.selected_i] {
+						v.p_x[idx] = v.b_x[v.selected_i]
+						v.p_y[idx] = v.b_y[v.selected_i] + i * v.input_spacing + v.input_spacing / 2
+						for l in v.p_link[idx] {
+							v.l_end_x[l] = v.p_x[idx]
+							v.l_end_y[l] = v.p_y[idx]
+						}
+					}
+					for i, idx in v.b_outs[v.selected_i] {
+						v.p_x[idx] = v.b_x[v.selected_i] + v.b_w[v.selected_i]
+						v.p_y[idx] = v.b_y[v.selected_i] + i * v.input_spacing + v.input_spacing / 2
+						for l in v.p_link[idx] {
+							v.l_start_x[l] = v.p_x[idx]
+							v.l_start_y[l] = v.p_y[idx]
+						}
+					}
+				}
+			}
+		}
+		.mouse_down {
+			if v.selected_i == -1 {
+				v.mouse_clicked = true
+				p_i := v.which_port_is_clicked(e.mouse_x, e.mouse_y)
+				if p_i != -1 {
+					// new link or select link
+					v.selected_variant = .link
+					// create link
+					v.p_link[p_i] << v.l_inp.len
+					v.selected_i = v.l_inp.len
+					if v.p_input[p_i] {
+						// we need an input for the link as the input for the block is the output for the link
+						v.l_start = true
+						v.l_out << p_i
+						v.l_inp << -1
+						v.l_end_x << v.p_x[p_i]
+						v.l_end_y << v.p_y[p_i]
+						v.l_start_x << e.mouse_x
+						v.l_start_y << e.mouse_y
+					} else {
+						v.l_start = false
+						v.l_inp << p_i
+						v.l_out << -1
+						v.l_start_x << v.p_x[p_i]
+						v.l_start_y << v.p_y[p_i]
+						v.l_end_x << e.mouse_x
+						v.l_end_y << e.mouse_y
+					}
+				} else {
+					l_i := v.which_link_is_clicked(e.mouse_x, e.mouse_y)
+					if l_i != -1 {
+						// select link
+						v.selected_variant = .link
+						v.selected_i = l_i
+						if e.mouse_x >= v.l_start_x[l_i] {
+							if (e.mouse_x - v.l_start_x[l_i]) >= (v.l_end_x[l_i] - e.mouse_x) {
+								v.l_start = false
+								idx := v.p_link[v.l_out[l_i]].index(l_i)
+								if idx != -1 {
+									v.p_link[v.l_out[l_i]].delete(idx)
+								}
+								v.l_out[l_i] = -1
+							} else {
+								v.l_start = true
+								idx := v.p_link[v.l_inp[l_i]].index(l_i)
+								if idx != -1 {
+									v.p_link[v.l_inp[l_i]].delete(idx)
+								}
+								v.l_inp[l_i] = -1
+							}
+						} else {
+							if (e.mouse_x - v.l_end_x[l_i]) <= (v.l_start_x[l_i] - e.mouse_x) {
+								v.l_start = false
+								idx := v.p_link[v.l_out[l_i]].index(l_i)
+								if idx != -1 {
+									v.p_link[v.l_out[l_i]].delete(idx)
+								}
+								v.l_out[l_i] = -1
+							} else {
+								v.l_start = true
+								idx := v.p_link[v.l_inp[l_i]].index(l_i)
+								if idx != -1 {
+									v.p_link[v.l_inp[l_i]].delete(idx)
+								}
+								v.l_inp[l_i] = -1
+							}
+						}
+					} else {
+						b_i := v.which_block_is_clicked(e.mouse_x, e.mouse_y)
+						if b_i != -1 {
+							// select block
+							v.selected_variant = .block
+							v.selected_i = b_i
+							v.b_dx = e.mouse_x - v.b_x[b_i]
+							v.b_dy = e.mouse_y - v.b_y[b_i]
+						}
+					}
+				}
+			}
+		}
+		.mouse_up {
+			v.mouse_clicked = false
+			if v.selected_i != -1 {
+				if v.selected_variant == .link {
+					p_i := v.which_port_is_clicked(e.mouse_x, e.mouse_y)
+					if p_i == -1 || p_i == v.l_out[v.selected_i]
+						|| p_i == v.l_inp[v.selected_i]
+						|| (p_i != -1 && v.p_input[p_i] == v.l_start) {
+						if v.l_inp[v.selected_i] != -1 {
+							idx := v.p_link[v.l_inp[v.selected_i]].index(v.selected_i)
+							if idx != -1 {
+								v.p_link[v.l_inp[v.selected_i]].delete(idx)
+							}
+						}
+						if v.l_out[v.selected_i] != -1 {
+							idx := v.p_link[v.l_out[v.selected_i]].index(v.selected_i)
+							if idx != -1 {
+								v.p_link[v.l_out[v.selected_i]].delete(idx)
+							}
+						}
+						v.l_end_x[v.selected_i] = -1.0
+						v.l_end_y[v.selected_i] = -1.0
+						v.l_start_x[v.selected_i] = -1.0
+						v.l_start_y[v.selected_i] = -1.0
+						v.l_inp[v.selected_i] = -1
+						v.l_out[v.selected_i] = -1
+					} else {
+						if v.p_input[p_i] {
+							v.p_link[p_i] << v.selected_i
+							v.l_end_x[v.selected_i] = v.p_x[p_i]
+							v.l_end_y[v.selected_i] = v.p_y[p_i]
+							v.l_out[v.selected_i] = p_i
+						} else {
+							v.p_link[p_i] << v.selected_i
+							v.l_start_x[v.selected_i] = v.p_x[p_i]
+							v.l_start_y[v.selected_i] = v.p_y[p_i]
+							v.l_inp[v.selected_i] = p_i
+						}
+					}
+				} else if v.selected_variant == .block {
+					v.b_x[v.selected_i] = e.mouse_x - v.b_dx
+					v.b_y[v.selected_i] = e.mouse_y - v.b_dy
+					for i, idx in v.b_inps[v.selected_i] {
+						v.p_x[idx] = v.b_x[v.selected_i]
+						v.p_y[idx] = v.b_y[v.selected_i] + i * v.input_spacing + v.input_spacing / 2
+						for l in v.p_link[idx] {
+							v.l_end_x[l] = v.p_x[idx]
+							v.l_end_y[l] = v.p_y[idx]
+						}
+					}
+					for i, idx in v.b_outs[v.selected_i] {
+						v.p_x[idx] = v.b_x[v.selected_i] + v.b_w[v.selected_i]
+						v.p_y[idx] = v.b_y[v.selected_i] + i * v.input_spacing + v.input_spacing / 2
+						for l in v.p_link[idx] {
+							v.l_start_x[l] = v.p_x[idx]
+							v.l_start_y[l] = v.p_y[idx]
+						}
+					}
+				}
+				v.selected_i = -1
+			}
+		}
+		else {}
 	}
 }
